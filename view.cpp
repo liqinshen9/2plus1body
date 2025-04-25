ab86aba52452076fefd722baf6e2d6a5a5252646
liqinshen9 <lshe727@aucklanduni.ac.nz>
2025-04-25 23:14:26 +1200

    added velocity on x direction. Two questions unsolved:why is the qDebug() run repeatedly, and why is there a strange behaviour happening in the bottom right?

--- view.cpp
+++ view.cpp
@@ -1,5 +1,7 @@
 #include "view.h"
 
+#include <QDebug>
+
 view::view(QQuickItem * parent):
     QQuickItem(parent)
 {
@@ -15,10 +17,16 @@ void view::initialize() {
     engine = qobject_cast<QQmlApplicationEngine*>(QQmlEngine::contextForObject(this)->engine());
     for (int i = 0; i <3;i++) {
         objects.append(createball());
+        break;
+
     }
     objects[0]->setPosition(QPointF(60,60));
-    objects[1]->setPosition(QPointF(200,60));
-    objects[2]->setPosition(QPointF(340,60));
+    objects[0]->setVelocity(QVector2D(5,0));
+    //objects[1]->setPosition(QPointF(200,60));
+    //objects[1]->setVelocity(QVector2D(5,0));
+    //objects[2]->setPosition(QPointF(340,60));
+    //objects[2]->setVelocity(QVector2D(0.5,0));
+
 }
 
 Ball* view::createball()
@@ -38,7 +46,15 @@ void view::iterate()
     for (int i =0;i<objects.length();i++) {
         QVector2D velocity = objects[i]->getVelocity();
         objects[i]->setPosition(objects[i]->position()+velocity.toPointF());
-        objects[i]->setVelocity(velocity+QVector2D(0,0.05));
+        objects[i]->setVelocity(velocity+QVector2D(0,0.05));//acceleration
+
+        if (objects[i]->position().y() + 100 > height() || objects[i]->position().x() + 100 > width()) {
+            objects[i]->setPosition(QPointF(width()-100, objects[i]->position().y()));
+            qDebug()<<222222222222222;
+            //objects[i]->setPosition(QPointF(objects[i]->position().x(), height()-100));
+
+            //objects[i]->setVelocity(objects[i]->getVelocity()*-0.9);
+        }
     }
 }
 
