2426e901035f8aca9ce203af89b6fdbefc205d8d
liqinshen9 <lshe727@aucklanduni.ac.nz>
2025-04-27 23:51:45 +1200

    change initial velocity in x and y direction, and print distnace whenever iterate() is called

--- view.cpp
+++ view.cpp
@@ -1,5 +1,5 @@
 #include "view.h"
-
+#include <QtMath>
 #include <QDebug>
 
 view::view(QQuickItem * parent):
@@ -15,15 +15,14 @@ view::view(QQuickItem * parent):
 void view::initialize() {
     //finds application engine pointer
     engine = qobject_cast<QQmlApplicationEngine*>(QQmlEngine::contextForObject(this)->engine());
-    for (int i = 0; i <3;i++) {
+    for (int i = 0; i <2;i++) {
         objects.append(createball());
-        break;
 
     }
     objects[0]->setPosition(QPointF(60,60));
-    objects[0]->setVelocity(QVector2D(5,0));
-    //objects[1]->setPosition(QPointF(200,60));
-    //objects[1]->setVelocity(QVector2D(5,0));
+    objects[0]->setVelocity(QVector2D(5,5));
+    objects[1]->setPosition(QPointF(200,60));
+    objects[1]->setVelocity(QVector2D(3,4));
     //objects[2]->setPosition(QPointF(340,60));
     //objects[2]->setVelocity(QVector2D(0.5,0));
 
@@ -43,11 +42,16 @@ Ball* view::createball()
 
 void view::iterate()//will run multiple times
 {
+    float resultx = qPow(objects[0]->position().x() - objects[1]->position().x(),2);
+    float resulty = qPow(objects[0]->position().y() - objects[1]->position().y(),2);
+    float distance = qSqrt(resultx+resulty);
+    qDebug()<< distance;
     for (int i =0;i<objects.length();i++) {
+
         QVector2D velocity = objects[i]->getVelocity();
         objects[i]->setPosition(objects[i]->position()+velocity.toPointF());
         objects[i]->setVelocity(velocity+QVector2D(0,0.05));//acceleration
-        qDebug()<<33333333;
+
         if (objects[i]->position().y() + 100 > height()){
             objects[i]->setPosition(QPointF(objects[i]->position().x(), height()-100));
             QVector2D v = objects[i]->getVelocity();
@@ -59,6 +63,14 @@ void view::iterate()//will run multiple times
             QVector2D v = objects[i]->getVelocity();
             QVector2D new_v = QVector2D(v.x()*-0.9,v.y());
             objects[i]->setVelocity(new_v);
+        }
+
+        if (objects[i]->position().x() < 0){
+            objects[i]->setPosition(QPointF(0,objects[i]->position().y()));
+            QVector2D v = objects[i]->getVelocity();
+            QVector2D new_v = QVector2D(v.x()*-0.9,v.y());
+            objects[i]->setVelocity(new_v);
+
         }
     }
 }
