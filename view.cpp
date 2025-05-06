6f24e269d0ddef9e2202817a8f3a1b6ab6a55758
liqinshen9 <lshe727@aucklanduni.ac.nz>
2025-05-06 23:47:54 +1200

    wip - improve efficiency of the program by adding nested for loop in iterate(), clean up duplicate code and program now allows for any number of balls

--- view.cpp
+++ view.cpp
@@ -15,17 +15,22 @@ view::view(QQuickItem * parent):
 void view::initialize() {
     //finds application engine pointer
     engine = qobject_cast<QQmlApplicationEngine*>(QQmlEngine::contextForObject(this)->engine());
-    for (int i = 0; i <3;i++) {
+    int n = 5;
+    for (int i = 0; i <n;i++) {
         objects.append(createball());
+        objects[i]->setPosition(QPointF(i * 600 / n, i * 500 / n + 50));
 
     }
-    objects[0]->setPosition(QPointF(60,60));
-    objects[0]->setVelocity(QVector2D(1,1));
-    objects[1]->setPosition(QPointF(200,60));
-    //objects[1]->setVelocity(QVector2D(3,4));
-    objects[2]->setPosition(QPointF(340,60));
-    //objects[2]->setVelocity(QVector2D(0.5,0));
 
+    //x: i * 600 / n
+    //y: i * 500 / n + 50
+
+    //objects[0]->setPosition(QPointF(60,60));
+    //objects[0]->setVelocity(QVector2D(1,1));
+    //objects[1]->setPosition(QPointF(200,60));
+    //objects[1]->setVelocity(QVector2D(3,4));
+    //objects[2]->setPosition(QPointF(340,60));
+    //objects[2]->setVelocity(QVector2D(0.5,0));
 }
 
 Ball* view::createball()
@@ -67,46 +72,72 @@ QVector2D view::acceleration(Ball * b0, Ball * b1)
 
 void view::iterate()//will run multiple times
 {
-    QColor b0_color = Qt::black;
-    QColor b1_color = Qt::black;
-    QColor b2_color = Qt::black;
-    QPointF p1 = objects[1]->position();
-    QPointF p0 = objects[0]->position();
-    QPointF p2 = objects[2]->position();
-    Ball * b1 = objects[1];
-    Ball * b0 = objects[0];
-    Ball * b2 = objects[2];
 
-    if(QVector2D(p1-p0).length() < 100){
-        b0_color = Qt::blue;
-        b1_color = Qt::blue;
+    int n = objects.size();
+    for (int i = 0; i < n; i++) {
+        QColor i_color = Qt::black;
+        QColor j_color = Qt::black;
+        for (int j = 0; j <n ;j++) {
+            if (i!=j) {
+                QVector2D a = acceleration(objects[i],objects[j]);
+                objects[i]->setVelocity(objects[i]->getVelocity() + a);
+                if(QVector2D(objects[i]->position()-objects[j]->position()).length() < 100){
+                    i_color = Qt::red;
+                    j_color = Qt::red;
+                }
+                objects[i]->getPaintball()->setProperty("color",i_color);
+                objects[j]->getPaintball()->setProperty("color",j_color);
+
+            } else {
+                continue;
+            }
+        }
+
+
     }
-    b0->getPaintball()->setProperty("color",b0_color);
-    b1->getPaintball()->setProperty("color",b1_color);
+    //QPointF p = objects[i]->position();
+    //Ball * b = objects[i];
+    //QColor b0_color = Qt::black;
+    //QColor b1_color = Qt::black;
+    //QColor b2_color = Qt::black;
+    //Ball * b1 = objects[1];:black;
+    //QPointF p1 = objects[1]->position();
+    //QPointF p0 = objects[0]->position();
+    //QPointF p2 = objects[2]->position();
+    //Ball * b1 = objects[1];
+    //Ball * b0 = objects[0];
+    //Ball * b2 = objects[2];
 
-    if(QVector2D(p2-p0).length() < 100){
-        b0_color = Qt::blue;
-        b2_color = Qt::blue;
-    }
-    b2->getPaintball()->setProperty("color",b2_color);
-    b0->getPaintball()->setProperty("color",b0_color);
-    if(QVector2D(p2-p1).length() < 100){
-        b1_color = Qt::blue;
-        b2_color = Qt::blue;
-    }
-    b2->getPaintball()->setProperty("color",b2_color);
-    b1->getPaintball()->setProperty("color",b1_color);
+    //if(QVector2D(p1-p0).length() < 100){
+        //b0_color = Qt::blue;
+        //b1_color = Qt::blue;
+    //}
+    //b0->getPaintball()->setProperty("color",b0_color);
+    //b1->getPaintball()->setProperty("color",b1_color);
 
-    QVector2D a01 = acceleration(b0,b1);
-    QVector2D a10 = acceleration(b1,b0);
-    QVector2D a02 = acceleration(b0,b2);
-    QVector2D a12 = acceleration(b1,b2);
-    QVector2D a20 = acceleration(b2,b0);
-    QVector2D a21 = acceleration(b2,b1);
+    //if(QVector2D(p2-p0).length() < 100){
+        //b0_color = Qt::blue;
+        //b2_color = Qt::blue;
+    //}
+    //b2->getPaintball()->setProperty("color",b2_color);
+    //b0->getPaintball()->setProperty("color",b0_color);
+    //if(QVector2D(p2-p1).length() < 100){
+        //b1_color = Qt::blue;
+        //b2_color = Qt::blue;
+    //}
+    //b2->getPaintball()->setProperty("color",b2_color);
+    //b1->getPaintball()->setProperty("color",b1_color);
 
-    b0->setVelocity(b0->getVelocity() + a01+a02);//Other is b1, this is b0
-    b1->setVelocity(b1->getVelocity() + a12+a10);
-    b2->setVelocity(b2->getVelocity() + a20+a21);
+    //QVector2D a01 = acceleration(b0,b1);
+    //QVector2D a10 = acceleration(b1,b0);
+    //QVector2D a02 = acceleration(b0,b2);
+    //QVector2D a12 = acceleration(b1,b2);
+    //QVector2D a20 = acceleration(b2,b0);
+    //QVector2D a21 = acceleration(b2,b1);
+
+    //b0->setVelocity(b0->getVelocity() + a01+a02);//Other is b1, this is b0
+    //b1->setVelocity(b1->getVelocity() + a12+a10);
+    //b2->setVelocity(b2->getVelocity() + a20+a21);
 
 
 
